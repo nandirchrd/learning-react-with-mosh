ae4ad970e0b017a99d1886b6e96b706ad173d496
nandirchrd <nandirchrd@gmail.com>
2022-09-04 23:50:57 +0700

    Added feature list genres

--- src/components/Movies/movies.jsx
+++ src/components/Movies/movies.jsx
@@ -1,14 +1,18 @@
 import { Component } from 'react';
 import { getMovies } from '../../services/fakeMovieService';
 import { Movie } from '../';
-import Pagination from '../Pagination/pagination';
+import Pagination from '../common/pagination';
 import paginate from '../../utils/paginate';
+import ListGroup from '../common/listGroup';
+import { getGenres } from '../../services/fakeGenreService';
 
 class Movies extends Component {
 	state = {
-		movies: null,
+		movies: [],
 		currentPage: 1,
 		pageSize: 4,
+		genres: [],
+		selectedGenre: null,
 	};
 
 	componentDidUpdate() {
@@ -19,7 +23,12 @@ class Movies extends Component {
 	}
 	componentDidMount() {
 		console.log('COMP - Did mount');
-		this.setState({ ...this.state, movies: getMovies() });
+		const genres = [{ name: 'All genres' }, ...getGenres()];
+		this.setState({
+			...this.state,
+			movies: getMovies(),
+			genres,
+		});
 	}
 	shouldComponentUpdate() {
 		console.log('COMP - Should Update?');
@@ -60,10 +69,19 @@ class Movies extends Component {
 		if (page < 1 || page > pageCount) return;
 		this.setState({ ...this.state, currentPage: page });
 	};
+	handleGenreSelect = (genre) => {
+		this.setState({ ...this.state, selectedGenre: genre, currentPage: 1 });
+	};
 	showMovies() {
-		const { movies } = this.state;
+		const { movies, selectedGenre } = this.state;
+		const filtered =
+			selectedGenre && selectedGenre._id
+				? movies.filter(
+						(movie) => movie.genre._id === selectedGenre._id
+				  )
+				: movies;
 		const paginateMovies = paginate(
-			movies,
+			filtered,
 			this.state.currentPage,
 			this.state.pageSize
 		);
@@ -72,40 +90,49 @@ class Movies extends Component {
 			return <p>There is no movies in the database</p>;
 		}
 		return (
-			<>
-				<p>
-					Showing {paginateMovies.length}{' '}
-					{paginateMovies.length === 1 ? 'movie' : 'movies'} in the
-					database
-				</p>
-				<table className='table'>
-					<thead>
-						<tr>
-							<th>Title</th>
-							<th>Genre</th>
-							<th>Stock</th>
-							<th>Rate</th>
-							<th colSpan={2}></th>
-						</tr>
-					</thead>
-					<tbody>
-						{paginateMovies?.map((movie) => (
-							<Movie
-								key={movie._id}
-								movie={movie}
-								onDelete={this.handleDeleteMovie}
-								onLiked={this.handleLikedMovie}
-							/>
-						))}
-					</tbody>
-				</table>
-				<Pagination
-					itemsCount={this.state.movies.length}
-					pageSize={this.state.pageSize}
-					currentPage={this.state.currentPage}
-					onPage={this.handlePage}
-				/>
-			</>
+			<div className='row'>
+				<div className='col-3'>
+					<ListGroup
+						items={this.state.genres}
+						onItemSelect={this.handleGenreSelect}
+						selectedItem={this.state.selectedGenre}
+					/>
+				</div>
+				<div className='col'>
+					<p>
+						Showing {filtered.length}{' '}
+						{filtered.length === 1 ? 'movie' : 'movies'} in the
+						database
+					</p>
+					<table className='table'>
+						<thead>
+							<tr>
+								<th>Title</th>
+								<th>Genre</th>
+								<th>Stock</th>
+								<th>Rate</th>
+								<th colSpan={2}></th>
+							</tr>
+						</thead>
+						<tbody>
+							{paginateMovies?.map((movie) => (
+								<Movie
+									key={movie._id}
+									movie={movie}
+									onDelete={this.handleDeleteMovie}
+									onLiked={this.handleLikedMovie}
+								/>
+							))}
+						</tbody>
+					</table>
+					<Pagination
+						itemsCount={filtered.length}
+						pageSize={this.state.pageSize}
+						currentPage={this.state.currentPage}
+						onPage={this.handlePage}
+					/>
+				</div>
+			</div>
 		);
 	}
 
